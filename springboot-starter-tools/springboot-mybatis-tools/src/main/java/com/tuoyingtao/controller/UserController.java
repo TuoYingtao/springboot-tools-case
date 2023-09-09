4a893633df26f65db3b2ba473f8b6b50bd749db9
TuoYingtao <tuoyingtao@163.com>
2023-09-09 18:22:17 +0800

    Commons Tools 请求日志模块

--- springboot-starter-tools/springboot-mybatis-tools/src/main/java/com/tuoyingtao/controller/UserController.java
+++ springboot-starter-tools/springboot-mybatis-tools/src/main/java/com/tuoyingtao/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.tuoyingtao.controller;
 
+import com.commons.compound.log.annotation.Log;
+import com.commons.compound.log.enums.BusinessType;
 import com.github.pagehelper.PageInfo;
 import com.tuoyingtao.entity.UserEntity;
 import com.tuoyingtao.service.UserService;
@@ -23,18 +25,21 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Log(title = "查询用户列表")
     @RequestMapping(value = "list", method = RequestMethod.GET)
     public Result userList(@RequestParam Map<String, Object> param) {
          PageInfo<UserEntity> userList = userService.getUserList(param);
         return Result.ok().put(userList);
     }
 
+    @Log(title = "新增用户", businessType = BusinessType.INSERT)
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public Result addUser(UserEntity userEntity) {
         Long userId = userService.insertUser(userEntity);
         return Result.ok().put("userId", userId);
     }
 
+    @Log(title = "查询用户详情")
     @RequestMapping(value = "detail", method = RequestMethod.GET)
     public Result getUserDetail(@RequestParam("userId") Long userId) {
         UserEntity userEntityDetail = userService.getUserDetail(userId);
