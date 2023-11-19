dd7817155015627d1e2c392ea2c7aa81ae67a7b8
TuoYingtao <tuoyingtao@163.com>
2023-11-19 23:18:02 +0800

    Glume Generator 新增线程池配置

---
+++ glume-generator/glume-generator-service/src/main/java/com/glume/generator/service/properties/ThreadPoolConfigProperties.java
@@ -0,0 +1,58 @@
+package com.glume.generator.service.properties;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * 线程池配置类
+ *
+ * @Author: TuoYingtao
+ * @Date: 2023-09-06 14:36:40
+ * @Version: v1.0.0
+*/
+@ConfigurationProperties(prefix = "glume.thread")
+public class ThreadPoolConfigProperties {
+
+    /**
+     * 核心线程数 默认：50
+     */
+    private Integer corePoolSize;
+    /**
+     * 最大线程数 默认：200
+     */
+    private Integer maximumPoolSize;
+    /**
+     * 活动时间 默认：10秒
+     */
+    private Integer keepAliveTime;
+
+    public ThreadPoolConfigProperties() {
+        this.corePoolSize = 50;
+        this.maximumPoolSize = 200;
+        this.keepAliveTime = 10;
+    }
+
+    public Integer getCorePoolSize() {
+        return corePoolSize;
+    }
+
+    public void setCorePoolSize(Integer corePoolSize) {
+        this.corePoolSize = corePoolSize;
+    }
+
+    public Integer getMaximumPoolSize() {
+        return maximumPoolSize;
+    }
+
+    public void setMaximumPoolSize(Integer maximumPoolSize) {
+        this.maximumPoolSize = maximumPoolSize;
+    }
+
+    public Integer getKeepAliveTime() {
+        return keepAliveTime;
+    }
+
+    public void setKeepAliveTime(Integer keepAliveTime) {
+        this.keepAliveTime = keepAliveTime;
+    }
+}
